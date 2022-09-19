786b8940c80d778df26ea65c76f225fe29f1832d
tramphuccuong <cuongb1910624@student.ctu.edu.vn>
2022-09-19 10:17:58 +0700

    Implement handlers

--- src/controllers/contact.controller.js
+++ src/controllers/contact.controller.js
@@ -20,3 +20,22 @@ exports.deleteAll  = (req,res) => {
 exports.findAllFavorite = (req,res) => { 
     return res.send({ message: 'findAllFavorite handler'}); 
 }; 
+const ContactService = require('../services/contact.service'); 
+const ApiError = require('../api-error');
+// Create and Save a new Contact
+exports.create = async (req, res, next) =>{
+     if (!req.body?. name) {
+return next(new ApiError(400, 'Name can not be empty'));
+}
+try {
+const contactService = new ContactService();
+const contact = await contactService.create(req.body);
+return res.send(contact); 
+} 
+catch (error) {
+console.log(error); 
+return next(
+new ApiError(500, 'An error occurred while creating the contact')
+);
+ }
+};
